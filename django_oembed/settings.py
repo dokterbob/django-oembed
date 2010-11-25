c48d4463189e4d0c861e6b431b0698dafe011012
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2010-11-25 16:00:49 +0100

    Removed reference to parent project Metaphore in settings.

--- django_oembed/settings.py
+++ django_oembed/settings.py
@@ -1,4 +1,4 @@
 from django.conf import settings
 
-OEMBED_MAX_WIDTH = getattr(settings, 'METAPHORE_OEMBED_MAX_WIDTH', None)
-OEMBED_MAX_HEIGHT = getattr(settings, 'METAPHORE_OEMBED_MAX_HEIGHT', None)
+OEMBED_MAX_WIDTH = getattr(settings, 'OEMBED_MAX_WIDTH', None)
+OEMBED_MAX_HEIGHT = getattr(settings, 'OEMBED_MAX_HEIGHT', None)
